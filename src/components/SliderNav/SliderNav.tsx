10b2d219fc163f9e159a1cd77c6a824a3cae89e2
radek-rozmus <radoslaw.rozmus@microsoft.wsei.edu.pl>
2021-03-15 13:06:34 +0100

    slider nav

--- src/components/SliderNav/SliderNav.tsx
+++ src/components/SliderNav/SliderNav.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import {useState} from 'react';
 import './SliderNav.sass';
  
 const SliderNav = () => {
+    
+    const [chosenSlide, setChosenSlide] = useState(3);
+
+    const choose1:string = chosenSlide===1?"":"-empty"
+    const choose2:string = chosenSlide===2?"":"-empty"
+    const choose3:string = chosenSlide===3?"":"-empty"
+
     return ( 
         <div className="slider-nav">
-            SliderNav
+            <div className={"chosen-slide icon-circle" + choose1} onClick={() => setChosenSlide(1)}></div>
+            <div className={"chosen-slide icon-circle" + choose2} onClick={() => setChosenSlide(2)}></div>
+            <div className={"chosen-slide icon-circle" + choose3} onClick={() => setChosenSlide(3)}></div>
         </div>
      );
 }
