0aff0b32ee829d16d3ca7af2ee6cd3b9b42010c4
radioDevCreations <radekrozmus@gmail.com>
2021-04-02 23:19:41 +0200

    form

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -1,12 +1,27 @@
+import * as React from 'react';
+import { Component } from 'react';
 import './ContactForm.sass';
 
  
-const ContactForm = () => {
-    return ( 
-        <p>
-            Contact Form
-        </p>
-     );
+export interface Props {
+    
+}
+ 
+export interface State {
+    
+}
+ 
+class ContactForm extends Component<Props, State> {
+    render() { 
+        return (  
+            <form className="contact-form">
+                <input type="email" placeholder="Your email" id="user-email" className="form-input"/>
+                <input type="text" placeholder="Your name" id="user-name" className="form-input"/>
+                <input type="text" placeholder="Subject..." id="subject" className="form-input"/>
+                <input type="text" placeholder="Message..." id="message" className="form-input"/>
+            </form>
+        );
+    }
 }
  
 export default ContactForm;
